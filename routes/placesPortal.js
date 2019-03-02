a206bba6e7db29918af6c2e29f8b6aa356b0fddb
taha <haqtaha@gmail.com>
2019-03-02 09:50:51 +0500

    added jwt

--- routes/placesPortal.js
+++ routes/placesPortal.js
@@ -1,12 +1,29 @@
 let  express = require('express'),
     router  = express.Router(),
+    jwt = require('jsonwebtoken'),
     db_read = require('../utils/db-functions/read'),
     db_insert = require('../utils/db-functions/insert'),
     db_update = require('../utils/db-functions/update'),
     db_delete = require('../utils/db-functions/delete'),
     constants = require("../utils/constants");
 
-router.put('/:place_id', function (req, res) {
+
+function verifyToken(req, res, next) {
+
+    let token = req.body['x-access-token'];
+    if (!token)
+        return res.status(403).send({
+            auth: false, message: 'No token provided.'});
+    jwt.verify(token, constants.secret, function (err, decoded) {
+        if (err)
+            return res.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+        // if everything good, save to request for use in other routes
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+router.put('/:place_id', verifyToken,function (req, res) {
     db_update.updatePlaceDetails(req.params.place_id, req.body).then((response) => {
         //SUCCESS
         res.status(201).send(
@@ -53,7 +70,6 @@ router.post('/place-login', function (req, res) {
         )
     });
 });
-
 
 
 router.post('/place-register', function (req, res) {
--- utils/db-functions/read.js
+++ utils/db-functions/read.js
@@ -27,22 +27,10 @@ exports.getPlaceById = async (placeId) => {
 
 
 
-////////////////////////--------------------------------------------- RESTAURANT PORTAL FUNCTION -----------------------------------------------/////////////////////////
-
-
-exports.getAllFeaturedPlacesLogin = async () => {
-    try {
-        return await restPortal.find({});
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
 
 exports.authenticateAdmin = async (user) => {
     try {
-        let admin = await placeModel.find({email: user.email});
+        let admin = await adminModel.find({email: user.email});
         if (utilFunctions.isEmpty(admin)) {
             throw new Error(constants.responseMessages.emailNotFound)
         }
@@ -68,9 +56,9 @@ exports.authenticateAdmin = async (user) => {
     }
 };
 
-exports.authenticateAdmin = async (user) => {
+exports.authenticatePlace = async (user) => {
     try {
-        let placeUser = await adminModel.find({email: user.email});
+        let placeUser = await placeModel.find({email: user.email});
         if (utilFunctions.isEmpty(placeUser)) {
             throw new Error(constants.responseMessages.emailNotFound)
         }
@@ -118,84 +106,6 @@ exports.authenticateUser = async (user) => {
 
         return {auth: true, token: token, user: returningUser}
 
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
-exports.getAllUsers = async () => {
-    try {
-        return await userModel.find({});
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
-
-//// FEATURED FUNCTIONS
-
-exports.getFoodFeatured = async () => {
-    try {
-        const foodFeaturedData = await foodFeatured.find({});
-        for (let i = 0; i < foodFeaturedData.length; i++) {
-            foodFeaturedData[i].opening_hours.open_now = true
-        }
-        return foodFeaturedData;
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
-exports.getFoodFeaturedHomepageCount = async () => {
-    try {
-        const count = await foodFeatured.find({isOnHomepage: true});
-        return count.length < 3;
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
-exports.getHomepageFoodFeatured = async () => {
-    try {
-        const foodFeaturedData = await foodFeatured.find({isOnHomepage: true});
-        for (let i = 0; i < foodFeaturedData.length; i++) {
-            foodFeaturedData[i].opening_hours.open_now = true
-        }
-        return foodFeaturedData;
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
-
-exports.getHomepageHangoutPlaces = async () => {
-    try {
-        const hangoutData = await hangoutModel.find({isOnHomepage: true});
-        for (let i = 0; i < hangoutData.length; i++) {
-            hangoutData[i].opening_hours.open_now = true
-        }
-        return hangoutData;
-    } catch (e) {
-        console.log(e);
-        throw new Error(e);
-    }
-};
-
-
-exports.getFoodFeaturedDetail = async (featuredDetailId) => {
-    try {
-        timeago.addLocale(en);
-        const timeAgo = new timeago('en-US');
-        let placeDetail = await foodFeaturedDetail.findById(featuredDetailId).populate('reviews');
-        for (let i = 0; i < placeDetail.reviews.length; i++) {
-            placeDetail.reviews[i].relative_time_description = timeAgo.format(date);
-        }
-        return placeDetail;
     } catch (e) {
         console.log(e);
         throw new Error(e);
